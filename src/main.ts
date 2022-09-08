8c746664f22a6492bc72e402dba7e80066e3740a
Govinda Mac <govi2010@gmail.com>
2022-09-08 23:00:58 +0530

    comint

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+import {platformNativeScript, registerElement, runNativeScriptAngularApp} from '@nativescript/angular';
+import {SVGView} from "@nativescript-community/ui-svg";
 
 import { AppModule } from './app/app.module';
-
+registerElement('SVGView', () => SVGView)
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
 });
