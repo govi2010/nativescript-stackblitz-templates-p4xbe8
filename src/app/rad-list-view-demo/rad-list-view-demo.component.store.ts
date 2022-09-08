a5b0b2ee40b9b92cbfd39cd7c092589448bd3759
govi2010 <govi2010@gmail.com>
2022-09-08 22:55:50 +0530

    5 files modified

--- src/app/rad-list-view-demo/rad-list-view-demo.component.store.ts
+++ src/app/rad-list-view-demo/rad-list-view-demo.component.store.ts
@@ -1,84 +1,123 @@
-import {ComponentStore, tapResponse} from "@ngrx/component-store";
-import {delay, Observable, of, switchMap, tap} from "rxjs";
-import {createEntityAdapter, EntityState} from "@ngrx/entity";
-import {Injectable} from "@angular/core";
-import {RouteLocation, Selectable} from "~/app/modal";
-import {data} from "~/app/data";
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { delay, Observable, of, switchMap, tap } from 'rxjs';
+import { createEntityAdapter, EntityState } from '@ngrx/entity';
+import { Injectable } from '@angular/core';
+import { RouteLocation, Selectable } from '~/app/modal';
+import { data } from '~/app/data';
 
 const adaptor = createEntityAdapter<Selectable<RouteLocation>>({
-    selectId: model => model.id,
-    sortComparer: (a: RouteLocation, b: RouteLocation) => {
-        let x = a.name.toLowerCase();
-        let y = b.name.toLowerCase();
-        if (x < y) {
-            return -1;
-        }
-        if (x > y) {
-            return 1;
-        }
-        return 0;
+  selectId: (model) => model.id,
+  sortComparer: (a: RouteLocation, b: RouteLocation) => {
+    let x = a.name.toLowerCase();
+    let y = b.name.toLowerCase();
+    if (x < y) {
+      return -1;
     }
+    if (x > y) {
+      return 1;
+    }
+    return 0;
+  },
 });
 
-const {selectAll, selectEntities, selectIds, selectTotal} = adaptor.getSelectors()
+const { selectAll, selectEntities, selectIds, selectTotal } =
+  adaptor.getSelectors();
 
 export class DailyRouteCreateComponentState {
-    selectableRoutLocation: EntityState<Selectable<RouteLocation>> = adaptor.getInitialState();
-    search: string = '';
-    isLoading: boolean = false;
+  selectableRoutLocation: EntityState<Selectable<RouteLocation>> =
+    adaptor.getInitialState();
+  search: string = '';
+  isLoading: boolean = false;
 }
 
 @Injectable()
 export class RadListViewDemoComponentStore extends ComponentStore<DailyRouteCreateComponentState> {
-    constructor() {
+  constructor() {
+    super(new DailyRouteCreateComponentState());
+  }
 
-        super(new DailyRouteCreateComponentState());
+  readonly isLoading$: Observable<boolean> = this.select((s) => s.isLoading);
+
+  readonly selectableRoutLocationsWithStartEnd$: Observable<
+    Selectable<RouteLocation>[]
+  > = this.select((s) =>
+    s.selectableRoutLocation ? selectAll(s.selectableRoutLocation) : []
+  );
+  readonly search$: Observable<string> = this.select((s) => s.search);
+
+  readonly setSearch = this.updater((state, value: string) => ({
+    ...state,
+    search: value,
+  }));
+
+  readonly getData = this.effect((req$) =>
+    req$.pipe(
+      tap((req) => {
+        console.log('getData');
+        this.setState((state) => ({ ...state, isLoading: true }));
+      }),
+      switchMap((req) => {
+        return of(data).pipe(
+          delay(1500),
+          tapResponse(
+            (response: Selectable<RouteLocation>[]) => {
+              console.log('getData', response.length);
+              this.setState((state) => ({
+                ...state,
+                isLoading: false,
+                selectableRoutLocation: adaptor.addMany(
+                  response,
+                  state.selectableRoutLocation
+                ),
+              }));
+            },
+            (error) => {
+              this.setState((state) => ({ ...state, isLoading: false }));
+            }
+          )
+        );
+      })
+    )
+  );
+  readonly setRoutLocations = this.updater(
+    (state, value: Selectable<RouteLocation>[]) => {
+      return {
+        ...state,
+        selectableRoutLocation: adaptor.addMany(
+          value,
+          state.selectableRoutLocation
+        ),
+      };
     }
+  );
 
+  readonly checkUnCheckLocation = this.updater(
+    (state, value: { id: string; checked: boolean }) => {
+      return {
+        ...state,
+        selectableRoutLocation: adaptor.updateOne(
+          {
+            id: value.id,
+            changes: { checked: value.checked },
+          },
+          state.selectableRoutLocation
+        ),
+      };
+    }
+  );
 
-    readonly isLoading$: Observable<boolean> = this.select(s => s.isLoading)
-
-    readonly selectableRoutLocationsWithStartEnd$: Observable<Selectable<RouteLocation>[]> = this.select(s => s.selectableRoutLocation ? selectAll(s.selectableRoutLocation) : []);
-    readonly search$: Observable<string> = this.select(s => s.search)
-
-    readonly setSearch = this.updater((state, value: string) => ({...state, search: value}))
-
-    readonly getData = this.effect((req$) => req$.pipe(
-        tap(req => {
-            this.setState(state => ({...state, isLoading: true}))
-        }),
-        switchMap(req => {
-            return of(data).pipe(delay(1500), tapResponse((response: Selectable<RouteLocation>[]) => {
-                this.setState(state => ({...state, isLoading: false,selectableRoutLocation: adaptor.addMany(response, state.selectableRoutLocation)}))
-            }, error => {
-                this.setState(state => ({...state, isLoading: false}))
-            }))
-        })
-    ))
-    readonly setRoutLocations = this.updater((state, value: Selectable<RouteLocation>[]) => {
-        return {
-            ...state,
-            selectableRoutLocation: adaptor.addMany(value, state.selectableRoutLocation)
-        }
-    })
-
-    readonly checkUnCheckLocation = this.updater((state, value: { id: string, checked: boolean }) => {
-        return {
-            ...state,
-            selectableRoutLocation: adaptor.updateOne({
-                id: value.id,
-                changes: {checked: value.checked}
-            }, state.selectableRoutLocation)
-        }
-    })
-
-    readonly checkUnCheckAll = this.updater((state, value: boolean) => {
-        return {
-            ...state,
-            selectableRoutLocation: adaptor.updateMany([...selectAll(state.selectableRoutLocation).map(s => ({
-                id: s.id,
-                changes: {checked: value}
-            }))], state.selectableRoutLocation)
-        }
-    })
+  readonly checkUnCheckAll = this.updater((state, value: boolean) => {
+    return {
+      ...state,
+      selectableRoutLocation: adaptor.updateMany(
+        [
+          ...selectAll(state.selectableRoutLocation).map((s) => ({
+            id: s.id,
+            changes: { checked: value },
+          })),
+        ],
+        state.selectableRoutLocation
+      ),
+    };
+  });
 }
--- src/app/rad-list-view-demo/rad-list-view-demo.component.ts
+++ src/app/rad-list-view-demo/rad-list-view-demo.component.ts
@@ -1,74 +1,97 @@
-import {Component, OnDestroy, OnInit, ViewChild,} from '@angular/core';
-import {RouterExtensions} from '@nativescript/angular';
-import {isAndroid, Screen} from '@nativescript/core';
-import {distinctUntilChanged, Observable, take, takeUntil} from "rxjs";
-import {isEqual} from "lodash-es";
-import {RadListViewDemoComponentStore} from "./rad-list-view-demo.component.store";
-import {BaseComponent} from "../basecomponent";
-import {AccountLocation, AccountScope, LocationType, RouteLocation, Selectable} from "../modal";
-import {RadListViewComponent} from "nativescript-ui-listview/angular";
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { RouterExtensions } from '@nativescript/angular';
+import { isAndroid, Screen } from '@nativescript/core';
+import { distinctUntilChanged, Observable, take, takeUntil } from 'rxjs';
+import { isEqual } from 'lodash-es';
+import { RadListViewDemoComponentStore } from './rad-list-view-demo.component.store';
+import { BaseComponent } from '../basecomponent';
+import {
+  AccountLocation,
+  AccountScope,
+  LocationType,
+  RouteLocation,
+  Selectable,
+} from '../modal';
+import { RadListViewComponent } from 'nativescript-ui-listview/angular';
 
 export const searchFunctionFactory = (search$: Observable<string>) => {
-    return (item: RouteLocation) => {
-        let searchText = ''
-        search$.pipe(take(1)).subscribe(data => searchText = data);
-        if (searchText?.length) {
-            return item.name?.toLowerCase()?.includes(searchText?.toLowerCase()) && (!(item as AccountLocation).scope || ((item as AccountLocation).scope === AccountScope.account))
-        }
+  return (item: RouteLocation) => {
+    let searchText = '';
+    search$.pipe(take(1)).subscribe((data) => (searchText = data));
+    if (searchText?.length) {
+      return (
+        item.name?.toLowerCase()?.includes(searchText?.toLowerCase()) &&
+        (!(item as AccountLocation).scope ||
+          (item as AccountLocation).scope === AccountScope.account)
+      );
+    } else {
+      return true;
     }
-}
+  };
+};
 
 @Component({
-    selector: 'AppRadListViewComponentDemo',
-    templateUrl: './rad-list-view-demo.component.html',
-    styleUrls: ['./rad-list-view-demo.component.scss'],
-    providers: [RadListViewDemoComponentStore]
+  selector: 'AppRadListViewComponentDemo',
+  templateUrl: './rad-list-view-demo.component.html',
+  styleUrls: ['./rad-list-view-demo.component.scss'],
+  providers: [RadListViewDemoComponentStore],
 })
-export class RadListViewComponentDemo extends BaseComponent implements OnInit, OnDestroy {
-    @ViewChild('ListElement') listElement: RadListViewComponent;
-    selectableRoutLocationsWithStartEnd$: Observable<Selectable<RouteLocation>[]> = this.store.selectableRoutLocationsWithStartEnd$.pipe(takeUntil(this.destroy$), distinctUntilChanged(isEqual));
-    isLoading$: Observable<boolean> = this.store.isLoading$.pipe(takeUntil(this.destroy$), distinctUntilChanged());
-    search$: Observable<string> = this.store.search$.pipe(takeUntil(this.destroy$), distinctUntilChanged())
-    searchFunction = searchFunctionFactory(this.search$);
-    trackByFn = (index, item) => item?.id;
+export class RadListViewComponentDemo
+  extends BaseComponent
+  implements OnInit, OnDestroy
+{
+  @ViewChild('ListElement') listElement: RadListViewComponent;
+  selectableRoutLocationsWithStartEnd$: Observable<
+    Selectable<RouteLocation>[]
+  > = this.store.selectableRoutLocationsWithStartEnd$.pipe(
+    takeUntil(this.destroy$),
+    distinctUntilChanged(isEqual)
+  );
+  isLoading$: Observable<boolean> = this.store.isLoading$.pipe(
+    takeUntil(this.destroy$),
+    distinctUntilChanged()
+  );
+  search$: Observable<string> = this.store.search$.pipe(
+    takeUntil(this.destroy$),
+    distinctUntilChanged()
+  );
+  searchFunction = searchFunctionFactory(this.search$);
+  trackByFn = (index, item) => item?.id;
 
+  constructor(
+    private router: RouterExtensions,
+    public store: RadListViewDemoComponentStore
+  ) {
+    super();
+  }
 
-    constructor(
-        private router: RouterExtensions,
-        public store: RadListViewDemoComponentStore,
-    ) {
-        super();
+  ngOnDestroy() {
+    super.ngOnDestroy();
+  }
+
+  ngOnInit() {
+    this.store.getData();
+  }
+
+  templateSelectorFunc(item: Selectable<RouteLocation>) {
+    if (item['type'] === LocationType.start) {
+      return 'start';
     }
-
-    ngOnDestroy() {
-        super.ngOnDestroy();
+    if (item['type'] === LocationType.end) {
+      return 'end';
     }
+    return 'regular';
+  }
 
-    ngOnInit() {
-        this.store.getData();
+  onSearch(args: any) {
+    const text = args.object.text || '';
+    this.store.setSearch(text);
+    if (this.listElement) {
+      this.listElement.listView.refresh();
     }
+  }
 
-    templateSelectorFunc(item: Selectable<RouteLocation>) {
-        if (item['type'] === LocationType.start) {
-            return 'start';
-        }
-        if (item['type'] === LocationType.end) {
-            return 'end';
-        }
-        return 'regular';
-    }
-
-    onSearch(args: any) {
-        const text = args.object.text || '';
-        this.store.setSearch(text);
-        if (this.listElement) {
-            this.listElement.listView.refresh();
-        }
-    }
-
-    checkUncheck(id, checked: boolean, type: LocationType) {
-        this.store.checkUnCheckLocation({id, checked})
-    }
-
+  checkUncheck(id, checked: boolean, type: LocationType) {
+    this.store.checkUnCheckLocation({ id, checked });
+  }
 }
-
