247038ae0d7a11bd9e53459f6d76b05d7e3b0391
yogesh <=>
2022-09-08 22:34:00 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core'
-import { Routes } from '@angular/router'
-import { NativeScriptRouterModule } from '@nativescript/angular'
+import {NgModule} from '@angular/core'
+import {Routes} from '@angular/router'
+import {NativeScriptRouterModule} from '@nativescript/angular'
+import {RadListViewComponentDemo} from "~/app/rad-list-view-demo/rad-list-view-demo.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+    {
+        path: '',
+        component: RadListViewComponentDemo
+    },
 ]
 
 @NgModule({
-  imports: [NativeScriptRouterModule.forRoot(routes)],
-  exports: [NativeScriptRouterModule],
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
