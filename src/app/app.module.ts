b225227dd2de38e24dd9c4864f09ae13d1855edf
govi2010 <govi2010@gmail.com>
2022-09-09 16:08:13 +0530

    CollectionView

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
-import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
-import {NativeScriptModule} from '@nativescript/angular';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from '@nativescript/angular';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NativeScriptUIListViewModule} from 'nativescript-ui-listview/angular';
-import {RadListViewComponentDemo} from "~/app/rad-list-view-demo/rad-list-view-demo.component";
-import {PushModule} from "@ngrx/component";
-import {CheckboxComponent} from "~/app/checkbox/checkbox.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NativeScriptUIListViewModule } from 'nativescript-ui-listview/angular';
+import { RadListViewComponentDemo } from '~/app/rad-list-view-demo/rad-list-view-demo.component';
+import { PushModule } from '@ngrx/component';
+import { CheckboxComponent } from '~/app/checkbox/checkbox.component';
+import { CollectionViewModule } from '@nativescript-community/ui-collectionview/angular';
 
 @NgModule({
-    bootstrap: [AppComponent],
-    imports: [NativeScriptModule, AppRoutingModule, NativeScriptUIListViewModule, PushModule],
-    declarations: [AppComponent, CheckboxComponent, RadListViewComponentDemo],
-    providers: [],
-    schemas: [NO_ERRORS_SCHEMA],
+  bootstrap: [AppComponent],
+  imports: [
+    NativeScriptModule,
+    AppRoutingModule,
+    NativeScriptUIListViewModule,
+    CollectionViewModule,
+    PushModule,
+  ],
+  declarations: [AppComponent, CheckboxComponent, RadListViewComponentDemo],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA],
 })
-export class AppModule {
-}
+export class AppModule {}
